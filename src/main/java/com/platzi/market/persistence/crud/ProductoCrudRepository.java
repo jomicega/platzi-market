f3a222348adfefd00c7a15eaffb6f6147d3553d3
Jose Ceron <josem.cerong@gmail.com>
2022-06-01 05:40:21 -0500

    Create first repository

--- src/main/java/com/platzi/market/persistence/crud/ProductoCrudRepository.java
+++ src/main/java/com/platzi/market/persistence/crud/ProductoCrudRepository.java
@@ -3,6 +3,11 @@ package com.platzi.market.persistence.crud;
 import com.platzi.market.persistence.entity.Producto;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ProductoCrudRepository extends CrudRepository<Producto, Integer> {
+import java.util.List;
+import java.util.Optional;
 
+public interface ProductoCrudRepository extends CrudRepository<Producto, Integer> {
+//    @Query(value = "SELECT * FROM productos WHERE id_category = ?", nativeQuery = true) --> Forma nativa
+    List<Producto> findByIdCategoriaOrderByNameAsc(int idCategoria);
+    Optional<List<Producto>> findByCantidadStockLessThanAndEstado(int cantidadStock, boolean estado);
 }
