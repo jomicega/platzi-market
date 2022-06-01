80012a2d009db6f923f89a9ff88560f4956dc571
Jose Ceron <josem.cerong@gmail.com>
2022-06-01 07:39:31 -0500

    repository oriented to domain

---
+++ src/main/java/com/platzi/market/persistence/mapper/CategoyMapper.java
@@ -0,0 +1,22 @@
+package com.platzi.market.persistence.mapper;
+
+import com.platzi.market.domain.Category;
+import com.platzi.market.persistence.entity.Categoria;
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+
+@Mapper(componentModel = "spring")
+public interface CategoyMapper {
+    @Mappings({
+            @Mapping(source = "idCategoria", target = "categoryId"),
+            @Mapping(source = "descripcion", target = "category"),
+            @Mapping(source = "estado", target = "active"),
+    })
+    Category toCategory(Categoria category);
+
+    @InheritInverseConfiguration //esta directiva indica que es lo inverso a lo que está arriba y no es necesario hacer un nuevo mapping
+    @Mapping(target = "productos", ignore = true)
+    Categoria toCategoria(Category category);
+}
